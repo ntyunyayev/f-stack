2cd3e51f0803019c52a5066d3763f14a761ff843
kikito <nikita.tyunyayev@student.uclouvain.be>
2023-04-27 08:40:41 +0000

    adding compil scripts

---
+++ app/nginx-1.16.1/mymake.sh
@@ -0,0 +1,3 @@
+bash ./configure --prefix=/usr/local/nginx_fstack --with-ff_module
+make
+sudo make install
---
+++ lib/mymake.sh
@@ -0,0 +1,2 @@
+make
+sudo PKG_CONFIG_PATH=/home/nikita/workspace/f-stack/dpdk/install/lib/x86_64-linux-gnu/pkgconfig:/usr/local/nginx_fstack/sbin/nginx make install
